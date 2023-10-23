955801833d19b08a78f695645ff680f3bcc3eb9b
Timerlan01 <alimbekovtimerlan@gmail.com>
2023-10-23 12:01:56 +0600

    Добавьте файлы проекта.

---
+++ Module08PracticeTask1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module08PracticeTask1
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            RangeOfArray arr = new RangeOfArray(6, 10);
+            arr[6] = 42;
+            arr[7] = 15;
+
+            for (int i = 6; i <= 10; i++)
+            {
+                Console.WriteLine($"arr[{i}] = {arr[i]}");
+            }
+        }
+    }
+}
---
+++ Module08PracticeTask1/RangeOfArray.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module08PracticeTask1
+{
+    public class RangeOfArray
+    {
+        private int lowerIndex;
+        private int upperIndex;
+        private int[] array;
+
+        public RangeOfArray(int lowerIndex, int upperIndex)
+        {
+            if (upperIndex < lowerIndex)
+            {
+                throw new ArgumentException("Upper index cannot be less than lower index.");
+            }
+
+            this.lowerIndex = lowerIndex;
+            this.upperIndex = upperIndex;
+            this.array = new int[upperIndex - lowerIndex + 1];
+        }
+
+        public int this[int index]
+        {
+            get
+            {
+                if (IsIndexValid(index))
+                {
+                    return array[index - lowerIndex];
+                }
+                throw new IndexOutOfRangeException("Index is out of range.");
+            }
+            set
+            {
+                if (IsIndexValid(index))
+                {
+                    array[index - lowerIndex] = value;
+                }
+                else
+                {
+                    throw new IndexOutOfRangeException("Index is out of range.");
+                }
+            }
+        }
+
+        private bool IsIndexValid(int index)
+        {
+            return index >= lowerIndex && index <= upperIndex;
+        }
+    }
+
+}
